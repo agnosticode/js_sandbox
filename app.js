2d7699518fcf5134e76b7749d8e6700c2e6296f3
Satinder Singh <agnosticode@gmail.com>
2018-03-11 22:05:32 +0530

    How to use the console in chrome and various functions of use

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-alert("hello"); 
+//Log to the console
+console.log("Hello console");//strings
+console.log(123);//numbers
+console.log(true);//booleans
+var greeting = "Hello";//variable declaration
+console.log(greeting);
+console.log([1,2,3,4,5]);//array
+console.log({a:1, b:2});//object
+
+console.table({a:1, b:2});//show in graphical form
+
+console.error("this is some error");//show error in the console
+
+console.warn("Beware of programmers");
+
+console.time("Time taken");
+    console.log("Hello console");
+    console.log("Hello console");
+    console.log("Hello console");
+    console.log("Hello console");
+    console.log("Hello console");
+    console.log("Hello console");
+    console.log("Hello console");
+    console.log("Hello console");
+    console.log("Hello console");
+    console.log("Hello console");
+console.timeEnd("Time taken");
+
+
+
+
+/* multi
+   line
+   comment 
+*/
