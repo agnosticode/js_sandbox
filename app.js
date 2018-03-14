80d1b34a7bd68233558492bcbe4dbaedbf1a0580
Satinder Singh <agnosticode@gmail.com>
2018-03-14 17:54:02 +0530

    Type conversion and coercion

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
-//PRIMITIVE DATA TYPES
+let val;
 
-//STRINGS
-const name='john doe';
-//NUMBER
-const age=45;
-//BOOLEAN
-const hasKids=true;
-//null
-const car=null;
-//undefined
-let test;
-//symbol
-const sym=Symbol();
+//Number to String
+val=String(5);
+val=String(4+4);
+//boolean to string
+val=String(true);
+//date to string
+val=String(new Date());
+//array to string
+val=String([1,2,3,4,5]);
+
+//toString Method
+val=(5).toString();
+val=(true).toString();
+
+//Strings to number
+val=Number('5');//5 
+//boolean to number
+val=Number(true);//1
+val=Number(false);//0    
+val=Number(null);//0
+val=Number('hello');//NaN-Not a number
+val=Number([1,2,3]);//NaN-Not a Number
+
+val=parseInt('100.23');//100
+val=parseFloat('100.23');//100.23
 
 
-//REFERENCE TYPES- OBJECTS
+//Output
+console.log(val);
+console.log(typeof val);
+//console.log(val.length);
+console.log(val.toFixed(2));
 
-//array
-const hobbies=['movies','music'];
-//objects literal
-const address={
-    city:'toronto',
-    state:'ontario'         
-};
-//dates
-const today=new Date();
+//Type Coercion
+const val1=String(5);
+const val2=6;
+const sum=val1+val2;//changed val2 into sting then concatinated it
 
-
-
-console.log(today);
-console.log(typeof today);
+console.log(sum);
+console.log(typeof sum);
