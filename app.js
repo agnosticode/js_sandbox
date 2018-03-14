c4211a823824618a417d0acc6decaa5a32660e00
Satinder Singh <agnosticode@gmail.com>
2018-03-15 01:34:15 +0530

    Template literals to write easy html in javascript

--- app.js
+++ app.js
@@ -1,64 +1,34 @@
-const firstName='William';
-const lastName='johnson';
-const age=23;
-const str='Hello there my name is Satinder';
-const tag='web design, web development, programming';
+const name='john';
+const age=30;
+const job='web developer';
+const city='toronto';
+let html;
 
-let val;
+function hello(){
+    return 'hello';
+}
 
-val=firstName+lastName;
-//concatination
-val=firstName+' '+lastName;
+//without template strings(es5)
+html='<ul><li>Name: '+name+'</li> <li>Age: '+age+'</li> <li>Job: '+job+'</li> <li>City: '+city+'</li></ul>';
 
-//Append
-val='Satinder ';
-val+='Singh';
-val='Hello my name is '+firstName+' and I am '+age;
+html='<ul>'+
+    '<li>Name: '+name+'</li>'+
+    '<li>Age: '+age+'</li>'+
+    '<li>Job: '+job+'</li>'+
+    '<li>City: '+city+'</li>'+
+    '</ul>';    
 
-//Escaping
-val='That\'s awesome, I can\'t wait'
+//with template strings(es6)
+html=`
+    <ul>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li>${2+2}</li>
+    <li>${hello()}</li>
+    <li>${age>30 ? 'Over 30' : 'Under 30'}</li>
+    </ul>
+`;
 
-//Properties and methods associated with string
-
-//Length(property)
-val=firstName.length;
-
-//concat(method)
-val=firstName.concat(' ',lastName);
-
-//Change case
-val=firstName.toUpperCase();
-val=firstName.toLocaleLowerCase();
-
-val=firstName[0];//W
-
-//indexOf()
-val=firstName.indexOf('l');//2
-val=firstName.lastIndexOf('l');//3
-
-//charAt()
-val=firstName.charAt(4);//i
-
-//Get last Char
-val=firstName.charAt(firstName.length-1);
-
-//substring()
-val=firstName.substring(0,4);//Will
-
-//slice()
-val=firstName.slice(0,4);//Will
-val=firstName.slice(-3);//iam
-
-//split()
-val=str.split(' ');//Array splited at space
-val=tag.split(',');//Array slplited at ,
-
-//replace()
-val=str.replace('Satinder','jack');
-
-//includes()
-val=str.includes('Hello');//true
-val=str.includes('foo');//false
-
-
-console.log(val);
+document.body.innerHTML = html;
