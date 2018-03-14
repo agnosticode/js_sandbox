437188407c5d32004cb81ef630bca93a3412ce6d
Satinder Singh <agnosticode@gmail.com>
2018-03-14 21:39:34 +0530

    String Methods and Concatenation

--- app.js
+++ app.js
@@ -1,29 +1,64 @@
-const num1=50;
-const num2=100;
+const firstName='William';
+const lastName='johnson';
+const age=23;
+const str='Hello there my name is Satinder';
+const tag='web design, web development, programming';
 
 let val;
 
-//Simple math with numbers
-val=num1+num2;
-val=num1*num2;
-val=num1-num2;
-val=num1/num2;
-val=num1%num2;//50
-val=num2%num1;//0
+val=firstName+lastName;
+//concatination
+val=firstName+' '+lastName;
 
-//Math Object
-val=Math.PI;
-val=Math.E;
-val=Math.round(6.596);
-val=Math.ceil(2.4);//3
-val=Math.floor(2.4);//2
-val=Math.sqrt(225);//15
-val=Math.abs(-3.65);//3.65
-val=Math.pow(8,2);//64
-val=Math.min(2,33,4,1,55,6,3);//1
-val=Math.max(2,33,4,1,55,6,3);//55
-val=Math.random();//decimal number 0<1
-val=Math.random()*20;//decimal number 0<20
-val=Math.floor(Math.random()*20);//whole number 0<20 
+//Append
+val='Satinder ';
+val+='Singh';
+val='Hello my name is '+firstName+' and I am '+age;
+
+//Escaping
+val='That\'s awesome, I can\'t wait'
+
+//Properties and methods associated with string
+
+//Length(property)
+val=firstName.length;
+
+//concat(method)
+val=firstName.concat(' ',lastName);
+
+//Change case
+val=firstName.toUpperCase();
+val=firstName.toLocaleLowerCase();
+
+val=firstName[0];//W
+
+//indexOf()
+val=firstName.indexOf('l');//2
+val=firstName.lastIndexOf('l');//3
+
+//charAt()
+val=firstName.charAt(4);//i
+
+//Get last Char
+val=firstName.charAt(firstName.length-1);
+
+//substring()
+val=firstName.substring(0,4);//Will
+
+//slice()
+val=firstName.slice(0,4);//Will
+val=firstName.slice(-3);//iam
+
+//split()
+val=str.split(' ');//Array splited at space
+val=tag.split(',');//Array slplited at ,
+
+//replace()
+val=str.replace('Satinder','jack');
+
+//includes()
+val=str.includes('Hello');//true
+val=str.includes('foo');//false
+
 
 console.log(val);
