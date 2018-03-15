ea1041a9d5957412b5a0644b0432e264ce2a4a9d
Satinder Singh <agnosticode@gmail.com>
2018-03-15 13:37:18 +0530

    Dates and Times

--- app.js
+++ app.js
@@ -1,38 +1,27 @@
-const person={
-    firstName:'Steve',
-    lastName:'Smith',
-    age:23,
-    email:'steve@gmail.com',
-    hobbies:['music','sports'],
-    address:{
-        city:'toronto',
-        state:'Ontario'
-    },
-    getBirthYear:function(){
-        return 2018-this.age;
-    }       
-};
-
 let val;
 
-val=person;//object
+const today=new Date();
+let birthday=new Date('7-25-1994 21:30:00');//mmddyyyy
+birthday=new Date('July 25 1994');
+birthday=new Date('07/25/1994');//mmddyyyy
 
-//Get specific value
-val=person.firstName;//Steve
-val=person['lastName'];//Another way
-val=person.age;
-val=person.hobbies[1];//sports
-val=person.address.city;//Toronto
-val=person.getBirthYear();
+val=today.getMonth();//2- one less than actual month
+val=today.getDate();//good it is actual 15
+val=today.getDay();//4 means thursday
+val=today.getFullYear();
+val=today.getMinutes();
+val=today.getSeconds();
+val=today.getMilliseconds();
+val=today.getTime();//time stamp
 
-console.log(val);
+//Set all this stuff
+birthday.setMonth(2);//march
+birthday.setDate(12);
+birthday.setFullYear(1985);
+birthday.setHours(3);
+birthday.setMinutes(30);
+birthday.setSeconds(16);
 
-const people=[
-    {name:'john',age:30},
-    {name:'smith',age:54}
-];
 
-for(let i=0;i<people.length;i++){
-    console.log(people[i].name);
-    console.log(people[i].age);
-};
+
+console.log(birthday);   
