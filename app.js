44a0c0226bca53b81a7046af905f2d746ab34954
Satinder Singh <agnosticode@gmail.com>
2018-03-15 18:08:24 +0530

    Functions declarations and expressions

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
-const color='blue';
+//FUNCTION DECLARATIONS
 
-switch(color){
-    case 'red':
-    console.log('Color is red');
-    break;
-    case 'blue':
-    console.log('Color is blue');
-    break;
-    default:
-    console.log('Color is not red or blue');
-    break;
+function greet(firstName='John', lastName='Doe'){
+    // if(typeof firstName==='undefined'){firstName='John'};//ES5 WAY
+    // if(typeof lastName==='undefined'){lastName='Doe'};//ES5 WAY  
+    //console.log('Hello');
+    return 'Hello '+ firstName +' '+ lastName;
+}
+
+console.log(greet('Steve','Smith')); 
+console.log(greet());
+
+
+//Fuction Expressions
+const square=function(x=3){
+    return x*x;     
 };
 
-let day;
-switch(new Date().getDay()){
-     case 0: 
-     day='Sunday'; 
-     break;
-     case 1: 
-     day='Monday'; 
-     break;
-     case 2: 
-     day='Tuesday'; 
-     break;
-     case 3: 
-     day='Wednesday'; 
-     break;
-     case 4: 
-     day='Thursday'; 
-     break;
-     case 5: 
-     day='Friday'; 
-     break;
-     case 6: 
-     day='Saturday'; 
-     break;
+// console.log(square(6));
+
+//Immediately invokable function expressions- IIFE's
+(function(){
+    console.log('IIFE RAN....');
+})();
+
+//pass arguments
+(function(name){
+    console.log('Hello '+name);
+})('Satinder');
+
+//PROPERTY METHODS
+const todo={
+    add:function(){
+        console.log('Add todo');
+    },
+    edit:function(id){
+        console.log(`Edit todo ${id}`);
+    }
 };
-console.log(`Today is ${day}`);
+//call
+todo.add();//Add todo
+todo.edit(56);//Edit todo 56
+
+//add function to object ouside of it
+todo.delete = function(){
+    console.log('Delete todo');
+};
+todo.delete();
