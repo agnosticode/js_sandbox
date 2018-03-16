84ee43b374e24e16faffc9c7a23983ca7f8fb02e
Satinder Singh <agnosticode@gmail.com>
2018-03-16 21:36:29 +0530

    Block Scope with let and const

--- app.js
+++ app.js
@@ -1,56 +1,27 @@
-//WINDOW METHODS /OBJECTS /POPERTIES
-window.console.log(123);
+//Global Scope 
+var a=1;
+let b=2;
+const c=3;
 
-//Alert()
-//alert('Hello World');
+// function test(){
+// var a=4;
+// let b=5;
+// const c=6;
+// console.log('Fuction Scope: ', a, b, c);
+// }
+// test();
 
-//Prompt
-//const input=prompt();
-
-//Confirm
-// if(confirm('Are you sure?')){
-//     console.log('Yes');
-// }else{
-//     console.log('No');
+// if(true){
+//     //BLOCK SCOPE
+//     var a=4;
+//     let b=5;
+//     const c=6;
+//     console.log('If Scope: ', a, b, c);
 // }
 
-let val;
+//Forloop
+for(let a=0; a<10; a++){
+    console.log(`Loop: ${a}`);
+}
 
-//Outer height and width
-val=window.outerHeight;
-val=window.outerWidth
-
-//Inner height and width
-val=window.innerHeight;
-val=window.innerWidth
-
-//Scroll
-val=window.scrollY;
-val=window.scrollX;
-
-//Location object
-val=window.location;
-val=window.location.hostname;
-val=window.location.port;
-val=window.location.href;
-val=window.location.search;
-
-//REDIRECT
-//val=window.location.href='http://www.google.com';
-
-//RELOAD
-//val=window.location.reload();
-
-//History Object
-//window.history.go(0);//-1,-2
-val=window.history.length;
-
-//Navigator Object
-val=window.navigator;
-val=window.navigator.appCodeName;//Mozilla
-val=window.navigator.appName;//Netscape
-val=window.navigator.appVersion;//5.0+gibrish
-
-
-console.log(val);
-
+console.log('Global Scope: ', a, b, c);
